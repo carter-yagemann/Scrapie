ee4833eb120067e7c37c641d9899a0733d75553e
Carter Yagemann <carter.yagemann@gmail.com>
2015-10-04 00:13:36 -0400

    Implemented job processing.

--- process_job.py
+++ process_job.py
@@ -15,3 +15,39 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+import config
+from lxml import html
+import requests
+import time
+import json
+import sys
+
+if __name__ == "__main__":
+    # Get job from file
+    if len(sys.argv) < 2:
+        sys.exit("Insufficient arguments")
+    
+    filename = sys.argv[1]
+    file = open(filename, "r")
+    job = json.loads(file.read())
+    file.close()
+    
+    # Fetch HTML page
+    page = requests.get(job[0]['url'])
+    tree = html.fromstring(page.text)
+    # Scrape data
+    date = time.strftime("%m/%d/%Y-%H%M%S")
+    data = []
+    for i in range(1, len(job)):
+        try:
+            value = tree.xpath('//' + job[i]['element'] + '[@' + job[i]['attribute'] + '="' + job[i]['value'] + '"]/text()')[0].strip()
+            data.append(value)
+        except:
+            print "Failed to scrape", job[i]['value']
+    # Write to file
+    output = open(filename.replace(config.job_dir, config.data_dir).replace(".job", ".csv"), "a")
+    for i in range(len(data) - 1):
+        output.write(data[i] + ', ')
+    output.write(data[len(data) - 1] + '\n')
+    output.close()
